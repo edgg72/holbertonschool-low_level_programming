8bc4f0a4aec67f6039d676f9e55e4e64b449c3ad
edgg72 <edgg72@hotmail.com>
2020-06-10 20:04:20 +0000

    adding 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,8 @@
 #include<stdio.h>
-
+/**
+*main - using printf in exercise 5
+*Return: - returning an int
+*/
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
