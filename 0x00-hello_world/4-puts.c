ab2ec438f6a92da97d7bcf4eb86bedd4162f9f98
edgg72 <edgg72@hotmail.com>
2020-06-10 19:33:38 +0000

    adding 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
+/*
+ *exercise 4 0x00 hello world
+ */
 #include <stdio.h>
 
 int main(void)
 {
-
+/*
+ *printing message
+ */
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 
