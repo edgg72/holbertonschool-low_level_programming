f40455fc83b182090d10a89edc535b460b91a572
edgg72 <edgg72@hotmail.com>
2020-06-11 19:41:08 +0000

    adding 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+*main - printing numbers
+*Return: returning an int
+*/
+int main(void)
+{
+int num;
+for (num = 0; num < 10; num++)
+{
+putchar(num);
+}
+putchar('\n');
+return (0);
+}
