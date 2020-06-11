e0d9fe19e92eee21a5c25f8bce37ddf10c586512
edgg72 <edgg72@hotmail.com>
2020-06-11 20:46:24 +0000

    adding 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+*main - print alphabet in reverse
+*Return: returning an int
+*/
+int main(void)
+{
+char c;
+for (c = 'z'; c >= 'a'; --c)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
