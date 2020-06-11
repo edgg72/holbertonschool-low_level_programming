37b6beb0b432b30fc663cf36502551148a606a34
edgg72 <edgg72@hotmail.com>
2020-06-11 19:12:30 +0000

    adding 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - showing the alphabet
+*Return: returning an int
+*/
+int main(void)
+{
+char alpha;
+for (alpha = 'a'; alpha <= 'z'; ++alpha)
+{
+if (alpha != 'q' && alpha != 'e')       
+putchar(alpha);
+}
+putchar('\n');
+return (0);
+}
