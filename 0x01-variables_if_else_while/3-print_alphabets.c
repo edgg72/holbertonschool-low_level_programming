6f006dc799fa203d306e86fc0429c46f86fc5a52
edgg72 <edgg72@hotmail.com>
2020-06-11 19:04:10 +0000

    adding 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*main - showing the alphabet
+*Return: returning an int
+*/
+int main(void)
+{
+char alpha;
+for (alpha = 'a'; alpha <= 'z'; ++alpha)
+{
+putchar(alpha);
+}
+for (alpha = 'A'; alpha <= 'Z'; ++alpha)
+{
+putchar(alpha);
+}
+putchar('\n');
+return (0);
+}
