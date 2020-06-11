be8c6bc777ab87accb5b42448221f9b0af61f6c5
edgg72 <edgg72@hotmail.com>
2020-06-11 21:13:08 +0000

    adding 10

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -6,9 +6,10 @@
 int main(void)
 {
 int i;
-for(i = 0; i < 100 ; i++){
-putchar((i/10)+'0');
-putchar((i%10)+'0');
+for (i = 0; i < 100 ; i++)
+{
+putchar((i / 10) + '0');
+putchar((i % 10) + '0');
 putchar(',');
 putchar(' ');
 }
