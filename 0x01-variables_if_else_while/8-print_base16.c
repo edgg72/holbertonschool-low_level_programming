02ba9fb977e87077557af5ea14ab83c65ce82f2d
edgg72 <edgg72@hotmail.com>
2020-06-12 00:12:00 +0000

    adding 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+*main - print 00 to 99
+*Return: returning an int
+*/
+int main(void)
+{
+long num;
+char a;
+
+for (num = 48; num < 58; num++)
+{
+putchar(num);
+}
+
+for (a = 'a'; a < 'g'; ++a)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
