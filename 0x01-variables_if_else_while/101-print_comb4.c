55e252de8b8881294416786513fce6d2c98d889e
edgg72 <edgg72@hotmail.com>
2020-06-12 15:51:56 +0000

    adding 101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+/**
+*main - printing combinations of two digits
+*Return: returning an int
+*/
+int main(void)
+{
+int i, j, k;
+for (i = 48; i < 57; i++)
+{
+j = i + 1;
+while (j < 58)
+{
+k = j + 1;
+while (k < 58)
+{
+putchar (i);
+putchar (j);
+putchar (k);
+if (i == 55)
+{
+break;
+}
+putchar (',');
+putchar (' ');
+k++;
+}
+j++;
+}
+}
+putchar('\n');
+return (0);
+}
