4fecf8f242760dbf21bb4309dcbc665c5b90b628
edgg72 <edgg72@hotmail.com>
2020-06-16 16:15:11 +0000

    adding 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+* _islower - check uppercase or otherwise.
+* @c: evaluation
+* Return: Always 0.
+*/
+
+int _islower(int c)
+{
+
+int result;
+if (c >= 'a' && c <= 'z')
+{
+result = 0;
+}
+else
+{
+result = 0;
+}
+
+return (result);
+}
