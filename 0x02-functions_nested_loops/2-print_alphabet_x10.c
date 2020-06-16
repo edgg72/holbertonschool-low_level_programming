0d01b298f49239a6cc26c2e273470efedf18d378
edgg72 <edgg72@hotmail.com>
2020-06-16 15:12:18 +0000

    adding 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+* print_alphabet_x10 - program that prints the alphabet 10 times
+* Return: void
+*/
+
+void print_alphabet_x10(void)
+{
+int i;
+char c;
+for (i = 0; i <= 10; i++)
+{
+for (c = 'a'; c <= 'z'; ++c)
+{
+_putchar(c);
+}
+
+_putchar('\n');
+}
+
+}
