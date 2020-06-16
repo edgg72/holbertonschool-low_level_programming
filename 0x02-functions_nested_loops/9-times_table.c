97715838fabd62e78cd049a81ef76d8140b231a3
edgg72 <edgg72@hotmail.com>
2020-06-16 19:01:17 +0000

    adding 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,16 +8,16 @@
 
 void times_table(void)
 {
-int i,j;
- for(i = 0; i < 10; i++)
-   {
-     for(j = 0; j < 10; j++)
-       {
-	 x = i * j;
-	 _putchar(x);
-	 _putchar(',');
-	 _putchar(' ');
-       }
-     _putchar('\n');
-   }
+int i, j, x;
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 10; j++)
+{
+x = i * j;
+_putchar(x);
+_putchar(',');
+_putchar(' ');
+}
+_putchar('\n');
+}
 }
