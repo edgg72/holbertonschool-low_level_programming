cd26014170a1318aa9202697a498981ca46895de
edgg72 <edgg72@hotmail.com>
2020-06-17 03:17:39 +0000

    adding 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,16 +7,14 @@
 
 void print_alphabet_x10(void)
 {
+char c;
 int i;
-int c;
-for (i = 0; i <= 10; i++)
+for (i = 0; i < 10; i++)
 {
-for (c = 'a'; c <= 'z'; ++c)
+for (c = 'a'; c <= 'z'; c++)
 {
 _putchar(c);
 }
-
 _putchar('\n');
 }
-
 }
