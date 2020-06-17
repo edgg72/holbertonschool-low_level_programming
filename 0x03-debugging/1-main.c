ca90d79dc6ccd995f7ebbeb7aa312f72d4cf5440
edgg72 <edgg72@hotmail.com>
2020-06-17 03:00:22 +0000

    addin 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,10 +10,13 @@ int main(void)
 int i;
 printf("Infinite loop incoming :(\n");
 i = 0;
-while (i < 10)
-{
-putchar(i); /*the infinite loop is here */
-}
+/**
+*while (i < 10)
+*{
+*putchar(i);
+* }
+*/
+
 printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
