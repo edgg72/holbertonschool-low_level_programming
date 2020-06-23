52246a9f2d056c7f141264e24153c6bbf1868a4a
edgg72 <edgg72@hotmail.com>
2020-06-23 16:36:06 +0000

    adding 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,34 +2,32 @@
 
 /**
 *  puts2 -  prints every other character of a string
-*
+*  @str: string
 * Return: void.
 */
 
 void puts2(char *str)
 {
-
-  int i = 0;
-  char c[10];
-  while (i < 11)
-    {
-      c[i] = *(str + i);
-      i++;
-    }
-  i = 0;
-  while (i < 11)
-    {
-      if (i == 0)
-	{
-	  _putchar(c[i]);
-	  i++;
-	}
-      if (i % 2 == 0)
-	{
-	  _putchar(c[i]);
-	}
-      i++;
-    }
-  _putchar('\n');
-
+int i = 0;
+char c[10];
+while (i < 11)
+{
+c[i] = *(str + i);
+i++;
+}
+i = 0;
+while (i < 11)
+{
+if (i == 0)
+{
+_putchar(c[i]);
+i++;
+}
+if (i % 2 == 0)
+{
+_putchar(c[i]);
+}
+i++;
+}
+_putchar('\n');
 }
