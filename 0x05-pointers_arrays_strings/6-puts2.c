7943f675a031f42c093de490158ebc2ff5d44852
edgg72 <edgg72@hotmail.com>
2020-06-23 16:33:54 +0000

    adding 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+*  puts2 -  prints every other character of a string
+*
+* Return: void.
+*/
+
+void puts2(char *str)
+{
+
+  int i = 0;
+  char c[10];
+  while (i < 11)
+    {
+      c[i] = *(str + i);
+      i++;
+    }
+  i = 0;
+  while (i < 11)
+    {
+      if (i == 0)
+	{
+	  _putchar(c[i]);
+	  i++;
+	}
+      if (i % 2 == 0)
+	{
+	  _putchar(c[i]);
+	}
+      i++;
+    }
+  _putchar('\n');
+
+}
