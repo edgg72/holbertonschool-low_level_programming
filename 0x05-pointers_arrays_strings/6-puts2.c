ee79b0d6a543acf4198544bb570ed35a0393407a
edgg72 <edgg72@hotmail.com>
2020-06-23 21:59:37 +0000

    adding 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,10 +9,23 @@
 void puts2(char *str)
 {
 int i = 0;
-while (*(str + i) != 0)
+int x = 0;
+ while(*(str + x) != '\0')
+   {
+     x++;
+   } 
+
+while (*(str + i) != '\0')
 {
 _putchar(*(str + i));
-i = i + 2;
+ if((i + 2) <= x )
+   {
+     i = i + 2;
+   }
+ else
+   {
+     break;
+   }
 }
 _putchar('\n');
 }
