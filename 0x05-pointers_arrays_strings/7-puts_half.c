02bbd0b2e097fd8a836efc0b4864429b43aeefe5
edgg72 <edgg72@hotmail.com>
2020-06-23 18:09:21 +0000

    adding 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+* puts_half -  prints half of a string
+* @str: string
+* Return: void.
+*/
+
+void puts_half(char *str)
+{
+int i = 0;
+int j;
+while (*(str + i) != '\0')
+{
+i++;
+}
+j = i / 2;
+if (i % 2 != 0)
+{
+j++;
+}
+while (j < i)
+{
+_putchar(*(str + j));
+j++;
+}
+_putchar('\n');
+}
