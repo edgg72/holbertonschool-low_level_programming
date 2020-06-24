26f5886cb5039e2409413adc8c73c1bd183aa2cb
edgg72 <edgg72@hotmail.com>
2020-06-24 01:25:23 +0000

    adding 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+* _strcpy - copies a pointer to another
+* @s1: pointer 1
+* @s2: pointer 2
+* Return: pointer.
+*/
+
+char *_strcpy(char *s2, char *s1)
+{
+int i = 0;
+while (*(s1 + i))
+{
+*(s2 + i) = *(s1 + i);
+i++;
+}
+*(s2 + i) = '\0';
+return (s2);
+}
