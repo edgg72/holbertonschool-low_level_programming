964c10de41602321750598a16b2cd1b46270853a
edgg72 <edgg72@hotmail.com>
2020-06-25 22:23:18 +0000

    adding 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,24 +2,20 @@
 
 /**
  * string_toupper - changes all lowercase letters of a string to uppercase
- *
+ * @x: pointer
  * Return: string
  */
 
 char *string_toupper(char *x)
 {
-
-  int i = 0;
-
-  while (*(x + i) != '\0')
-    {
-      if (*(x + i) >= 97 && *(x + i) <= 122 )
-	{
-	  *(x + i) = *(x + i) - 32;
-	}
-      i++;
-    }
-    
-  return(x);
-
+int i = 0;
+while (*(x + i) != '\0')
+{
+if (*(x + i) >= 97 && *(x + i) <= 122)
+{
+*(x + i) = *(x + i) - 32;
+}
+i++;
+}
+return (x);
 }
