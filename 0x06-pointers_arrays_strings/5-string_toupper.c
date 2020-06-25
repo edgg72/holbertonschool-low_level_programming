d3446fd154a3e55cbb196553f8af190de6fbe851
edgg72 <edgg72@hotmail.com>
2020-06-25 22:20:54 +0000

    adding 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ *
+ * Return: string
+ */
+
+char *string_toupper(char *x)
+{
+
+  int i = 0;
+
+  while (*(x + i) != '\0')
+    {
+      if (*(x + i) >= 97 && *(x + i) <= 122 )
+	{
+	  *(x + i) = *(x + i) - 32;
+	}
+      i++;
+    }
+    
+  return(x);
+
+}
