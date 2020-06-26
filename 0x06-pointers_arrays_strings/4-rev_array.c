e54d3905ca44091f985be7011b589c92201bf99d
edgg72 <edgg72@hotmail.com>
2020-06-26 21:33:52 +0000

    fixing betty style

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * reverse_array - reverses array
+ * @a: integer
+ * @n: length of array
+ *
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+int i = 0, t;
+n = n - 1;
+while (i < n)
+{
+t = *(a + i);
+*(a + i) = *(a + n);
+*(a + n) = t;
+i++;
+n--;
+}
+}
