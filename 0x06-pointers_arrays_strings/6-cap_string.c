943c7b5c0e4de16ebf248aaa3d55a34873725a4e
edgg72 <edgg72@hotmail.com>
2020-06-26 03:13:06 +0000

    adding 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+
+/**
+ * cap_string - capitalizes starting letters
+ * @x: string
+ * Return: string
+ */
+
+char *cap_string(char *x)
+{
+int i = 0;
+while (*(x + i) != '\0')
+{
+if (*(x + i) == ',' || *(x + i) == ' ' || *(x + i) == 9)
+{
+if (*(x + (i + 1)) >= 'a' && *(x + (i + 1)) <= 'z')
+*(x + (i + 1)) = *(x + (i + 1)) - 32;
+}
+else if (*(x + i) == 10 || *(x + i) == ';' || *(x + i) == '.')
+{
+if (*(x + (i + 1)) >= 'a' && *(x + (i + 1)) <= 'z')
+*(x + (i + 1)) = *(x + (i + 1)) - 32;
+}
+else if (*(x + i) == '!' || *(x + i) == '?' || *(x + i) == '"')
+{
+if (*(x + (i + 1)) >= 'a' && *(x + (i + 1)) <= 'z')
+*(x + (i + 1)) = *(x + (i + 1)) - 32;
+}
+else if (*(x + i) == '(' || *(x + i) == ')')
+{
+if (*(x + (i + 1)) >= 'a' && *(x + (i + 1)) <= 'z')
+*(x + (i + 1)) = *(x + (i + 1)) - 32;
+}
+else if (*(x + i) == '{' || *(x + i) == '}')
+{
+if (*(x + (i + 1)) >= 'a' && *(x + (i + 1)) <= 'z')
+*(x + (i + 1)) = *(x + (i + 1)) - 32;
+}
+i++;
+}
+return (x);
+}
