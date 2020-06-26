d459e5093c1538de89fcd8de4082e9492473f093
edgg72 <edgg72@hotmail.com>
2020-06-26 00:50:30 +0000

    adding 8

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * rot13 - encodes to rot13
+ * @x: string
+ * Return: string
+ */
+
+char *rot13(char *x)
+{
+int i = 0, j = 0;
+char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+while (*(x + i) != '\0')
+{
+j = 0;
+while (j <= 52)
+{
+if (alpha[j] == *(x + i))
+{
+*(x + i) = rot[j];
+j++;
+break;
+}
+j++;
+}
+i++;
+}
+return (x);
+}
