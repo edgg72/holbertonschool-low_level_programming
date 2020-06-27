a6828f72990153458e5eaa649b428c41209820d3
edgg72 <edgg72@hotmail.com>
2020-06-27 16:24:25 +0000

    fixing betty style

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,27 +1,28 @@
 #include "holberton.h"
 /**
-* _strncpy - copies string to another
-* @dest: string1
-* @src: string2
-* @n: how many bytes to cpy
-*
-* Return: address of dest
-*/
-
+ * _strncpy - copies one string (or bytes given)
+ * @dest: where to copy;
+ * @src: string to copy or bytes to copy
+ * @n: how many bytes
+ * Return: dest
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
-int i;
+int i, j;
 i = 0;
-while (i < n && *(src + i))
+j = 0;
+while (i != n)
 {
-*(dest + i) = *(src + i);
+dest[j] = src[i];
+if (src[i] == '\0')
+{
+dest[j] = '\0';
+break;
+}
+j++;
 i++;
 }
-while (i < n)
-{
-*(dest + i) = '\0';
-i++;
-}
+while (j != n)
+dest[j++] = '\0';
 return (dest);
-}
 }
