bc8a22c682d37cd1b1dbce434ddb03e1cbf91207
edgg72 <edgg72@hotmail.com>
2020-06-28 15:16:10 +0000

    fixing betty

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -3,38 +3,40 @@
 
 /**
  * _strpbrk - searches a string for any of a set of bytes
- *
+ * @s: string to be scanned
+ * @accept: string
  * Return: pointer to the byte s that matches one of the bytes in accept
  */
 
 char *_strpbrk(char *s, char *accept)
 {
-
-  int i = 0;
-  int j = 0;
-  while (*(s + i) != '\0')
-    {
-      j = 0;
-      while (*(accept + j) != '\0')
-	{
-	  if (*(s + i) == *(accept + j))
-	    {
-	      break;
-	    }
-	  j++;
-	}
-      if (*(s + i) == *(accept + j))                                                                                                                                                                                      {
-	break;                                                                                                                                                                                                          }
-      i++;
-    }
-
-  if (*(s + i) == *(accept + j))                                                                                                                                                                                      {
-    return ((s + i));
-     }
-  else
-    {
-      return ('\0');
-    }
+int i = 0;
+int j = 0;
+while (*(s + i) != '\0')
+{
+j = 0;
+while (*(accept + j) != '\0')
+{
+if (*(s + i) == *(accept + j))
+{
+break;
+}
+j++;
+}
+if (*(s + i) == *(accept + j))
+{
+break;
+}
+i++;
+}
+if (*(s + i) == *(accept + j))
+{
+return ((s + i));
+}
+else
+{
+return ('\0');
+}
 
 
 }
