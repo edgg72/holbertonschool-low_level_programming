206b3007dc5bf1e147e12cc1b8dff7d22afbefec
edgg72 <edgg72@hotmail.com>
2020-06-29 13:18:37 +0000

    adding 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * _strspn -gets the length of a prefix substring
+ * _strpbrk - searches a string for any of a set of bytes.
  * @s: string
  * @accept: substring
  * Return: substring.
