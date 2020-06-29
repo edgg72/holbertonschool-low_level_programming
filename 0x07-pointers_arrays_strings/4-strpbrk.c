84ac68ba9ae8a0f2cbeeea1c57451b6c2b42070a
edgg72 <edgg72@hotmail.com>
2020-06-29 13:11:02 +0000

    adding 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,42 +1,25 @@
 #include "holberton.h"
 
-
 /**
- * _strpbrk - searches a string for any of a set of bytes
- * @s: string to be scanned
- * @accept: string
- * Return: pointer to the byte s that matches one of the bytes in accept
+ * _strspn -gets the length of a prefix substring
+ * @s: string
+ * @accept: substring
+ * Return: substring.
  */
 
-char *_strpbrk(char *s, char *accept)
-{
-int i = 0;
-int j = 0;
-while (*(s + i) != '\0')
-{
-j = 0;
-while (*(accept + j) != '\0')
-{
-if (*(s + i) == *(accept + j))
-{
-break;
-}
-j++;
-}
-if (*(s + i) == *(accept + j))
-{
-break;
-}
-i++;
-}
-if (*(s + i) == *(accept + j))
-{
-return ((s + i));
-}
-else
-{
-return ('\0');
-}
+unsigned int _strspn(char *s, char *accept)
 
-
+{
+  unsigned int i, j;
+  for (i = 0; s[i]; i++)
+    {
+      for (j = 0; accept[j]; j++)
+	{
+	  if (s[i] == accept[j])
+	    break;
+	}
+      if (accept[j] != '\0')
+	return (s + i);
+    }
+  return (i);
 }
