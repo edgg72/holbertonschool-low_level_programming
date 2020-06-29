b461ac21743a7f110742ac514b4d72dee6a2e42a
edgg72 <edgg72@hotmail.com>
2020-06-29 13:12:53 +0000

    adding 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,16 +10,16 @@
 unsigned int _strspn(char *s, char *accept)
 
 {
-  unsigned int i, j;
-  for (i = 0; s[i]; i++)
-    {
-      for (j = 0; accept[j]; j++)
-	{
-	  if (s[i] == accept[j])
-	    break;
-	}
-      if (accept[j] != '\0')
-	return (s + i);
-    }
-  return (i);
+unsigned int i, j;
+for (i = 0; s[i]; i++)
+{
+for (j = 0; accept[j]; j++)
+{
+if (s[i] == accept[j])
+break;
+}
+if (accept[j] != '\0')
+return (s + i);
+}
+return (0);
 }
