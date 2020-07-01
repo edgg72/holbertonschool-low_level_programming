10015440e971693fdc8ff03c63d7eee64534a416
edgg72 <edgg72@hotmail.com>
2020-07-01 19:04:50 +0000

    adding header, readme, 0 and 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion -  prints a string in reverse
+ * @s: string
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+if (*s == '\0')
+{
+_putchar('\n');
+return;
+}
+_puts_recursion(s + 1);
+_putchar(*s);
+}
