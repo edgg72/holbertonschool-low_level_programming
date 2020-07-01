b2e084ae0626d661080e482f0eb7485bf8592b51
edgg72 <edgg72@hotmail.com>
2020-07-01 21:17:10 +0000

    adding 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * _pow_recursion - x to the power of y
+ * @x: number 1
+ * @y: number 2
+ *
+ * Return: result
+ */
+
+int _pow_recursion(int x, int y)
+
+{
+if (y < 0)
+{
+return (-1);
+}
+if (y == 0)
+{
+return (1);
+}
+return (x * _pow_recursion(x, y - 1));
+}
