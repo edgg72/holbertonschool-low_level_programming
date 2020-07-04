deb7c6ed8599c9045de12b687b204cc4401ce129
edgg72 <edgg72@hotmail.com>
2020-07-04 19:49:06 +0000

    adding 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - pmultiplies two numbers.
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+int result;
+result = atoi(argv[1]) * atoi(argv[2]);
+if (argc != 3)
+{
+printf("Error");
+}
+else
+{
+printf("%d\n", result);
+}
+return (0);
+}
