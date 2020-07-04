6b8515d8627dd43f32ccb2a4c7f5bc87cfe084bc
edgg72 <edgg72@hotmail.com>
2020-07-04 18:04:27 +0000

    adding 0 and Readme

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - prints name of file
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+(void) argc;
+printf("%s\n", argv[0]);
+return (0);
+}
