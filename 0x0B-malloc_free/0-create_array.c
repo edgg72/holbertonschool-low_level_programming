1b81f1384d0c6dafbf2b792885bf74e93d6f515a
edgg72 <edgg72@hotmail.com>
2020-07-07 21:14:17 +0000

    adding 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,11 +12,19 @@
 char *create_array(unsigned int size, char c)
 {
 unsigned int i;
-char *p = (char *)malloc(size * sizeof(char));
+char *p;
+
 if (size == 0)
 {
 return (NULL);
 }
+
+p = (char *)malloc(size * sizeof(char));
+if (p == NULL)
+{
+return (NULL);
+}
+
 for (i = 0; i < size; i++)
 {
 p[i] = c;
