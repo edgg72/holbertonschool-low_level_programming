5194ccfc816c8bb96f37a151e7026dbde414a905
edgg72 <edgg72@hotmail.com>
2020-07-09 19:19:43 +0000

    adding 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: integer parameter
+ *
+ * Return: pointer to allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+
+int *p;
+p = malloc(b);
+if (p == NULL)
+{
+exit(98);
+}
+return (p);
+}
