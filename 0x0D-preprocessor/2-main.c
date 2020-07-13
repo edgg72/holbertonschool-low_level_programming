0c16453b6b46dc91e337acb59670601eee6d8a6b
edgg72 <edgg72@hotmail.com>
2020-07-13 20:19:09 +0000

    updating README and uploading all the exercises

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+/**
+ * main - prints the name of the file it was compiled from
+ *
+ * Return: void
+ *
+ */
+
+
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
