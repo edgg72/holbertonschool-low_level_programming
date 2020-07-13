70b2054d98ed17b5fef5ac6f76fdecda1cf0d1b9
edgg72 <edgg72@hotmail.com>
2020-07-13 21:27:32 +0000

    adding Readme file, dog.h, 1 and 2

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+include "dog.h"
+
+/**
+ * init_dog - initializes a structure of dog type
+ * @d: pointer to struct
+ * @name: pointer to the name of dog
+ * @age: age of the dog
+ * @owner: pointer owner of dog
+ *
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (d)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
+}
---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,11 @@
+project 0x0D. C - Structures, typedef
+
+dog.h - header file with function prototypes
+
+1-init_dog.c - functoin that initializes a variable of type struct dog.
+
+2-print_dog.c - function that prints a struct dog.
+
+4-new_dog.c - function that creates a new dog_t.
+
+5-free_dog.c - function that frees a dog_t.
