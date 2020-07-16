9d0109a3e6d39d3bb06a955f4225abd387d4983c
edgg72 <edgg72@hotmail.com>
2020-07-16 00:52:39 +0000

    fixing betty style

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,4 +1,4 @@
-#include "3-calc.h"  
+#include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -13,27 +13,26 @@
 int main(int argc, char *argv[])
 {
 
-  int a, b, c;
-  int (*f)(int, int);
+int arg1, arg3, r;
+int (*f)(int, int);
 
-   
-  if (argc != 4)
-    {
-      printf("Error\n");
-      exit(98);
-    }
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
 
-  a = atoi(argv[1]);
-  b = atoi(argv[3]); 
-  f = get_op_func(argv[2]); 
-  
-  
-  if (f == NULL)
-    {
-      printf("Error\n");
-      exit(99);
-    }
-  c = f(a, b);
-  printf("%d\n", c);
-  return (0);
+arg1 = atoi(argv[1]);
+arg3 = atoi(argv[3]);
+f = get_op_func(argv[2]);
+
+if (f == NULL)
+{
+printf("Error\n");
+exit(99);
+}
+
+r = f(arg1, arg3);
+printf("%d\n", r);
+return (0);
 }
