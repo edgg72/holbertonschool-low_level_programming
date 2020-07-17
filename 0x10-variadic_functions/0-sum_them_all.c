f729bd442aa71c387797cd44ec29141e62c0a7e7
edgg72 <edgg72@hotmail.com>
2020-07-17 17:13:00 +0000

    adding readme file, header file and 0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - returns the sum of all its parameters
+ * 
+ * Return:  the sum of all its parameters, if n == 0 return 0
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+
+  va_list valist;
+  int  sum = 0;
+  unsigned int i;
+
+  if(n == 0)
+    return(0);
+    
+  
+  va_start(valist, n);
+  
+  for(i = 0; i < n; i++)
+    {
+      sum = sum + va_arg(valist, int);
+    }
+
+  va_end(valist);
+
+  return (sum);
+
+}
