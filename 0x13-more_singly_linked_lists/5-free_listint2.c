9c75468b8ef28a4635da6f84ff70dc1e35e6ae9a
edgg72 <edgg72@hotmail.com>
2020-07-28 16:43:37 +0000

    adding aux variable

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,13 +9,16 @@
 
 void free_listint(listint_t *head)
 {
+	listint_t *aux;
+
 	if (head == NULL)
 		return;
 
 	while (head != NULL)
 	{
+		aux = (*head)->next;
 		free(head);
-		head = head->next;
+		*head = aux;
 	}
 	head = NULL;
 }
