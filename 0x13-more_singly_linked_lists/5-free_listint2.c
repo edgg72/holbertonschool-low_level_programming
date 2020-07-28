d55b0fcde86918a22e455f396a67726c91020117
edgg72 <edgg72@hotmail.com>
2020-07-28 20:16:47 +0000

    was missing a 2 in the function

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 
 /**
- * free_listint -  frees a listint_t list.
+ * free_listint2 -  frees a listint_t list.
  * @head: head
  * Return: void
  */
 
-void free_listint(listint_t *head)
+void free_listint2(listint_t **head)
 {
 	listint_t *aux;
 
