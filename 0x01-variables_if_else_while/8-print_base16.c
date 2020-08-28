92aa8a8db7ac82850b7dd3cce53e02c2bb662afd
edgg72 <edgg72@hotmail.com>
2020-08-28 12:12:59 -0500

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,22 +1,26 @@
 #include<stdio.h>
+
 /**
 *main - print 00 to 99
 *Return: returning an int
 */
+
 int main(void)
 {
+  
 long num;
 char a;
 
-for (num = 48; num < 58; num++)
-{
-putchar(num);
-}
+  for (num = 48; num < 58; num++)
+  {
+    putchar(num);
+  }
 
-for (a = 'a'; a < 'g'; ++a)
-{
-putchar(a);
-}
-putchar('\n');
-return (0);
+  for (a = 'a'; a < 'g'; ++a)
+  {
+    putchar(a);
+  }
+
+  putchar('\n');
+  return (0);
 }
