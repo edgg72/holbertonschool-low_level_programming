d81ca5b24b96bf2083b7acda171eff75ecf7c094
edgg72 <edgg72@hotmail.com>
2020-08-28 12:27:38 -0500

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,5 +1,6 @@
 #include "holberton.h"
 #include <ctype.h>
+
 /**
 * _islower - check uppercase or otherwise.
 * @c: evaluation
@@ -9,15 +10,12 @@
 int _islower(int c)
 {
 
-
-if (c >= 'a' && c <= 'z')
-{
-return (1);
-}
-else
-{
-return (0);
-}
-
-
+  if (c >= 'a' && c <= 'z')
+  {
+    return (1);
+  }
+  else
+  {
+    return (0);
+  }
 }
