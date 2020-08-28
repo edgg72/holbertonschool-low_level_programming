40beb5710f2ca7986b363d1c4b927515c5edc90f
edgg72 <edgg72@hotmail.com>
2020-08-28 12:14:41 -0500

    Update 10-print_comb2.c

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -1,21 +1,26 @@
 #include<stdio.h>
+
 /**
 *main - print 00 to 99
 *Return: returning an int
 */
+
 int main(void)
 {
-int i;
-for (i = 0; i < 100 ; i++)
-{
-putchar((i / 10) + '0');
-putchar((i % 10) + '0');
-if (i != 99)
-{
-putchar(',');
-putchar(' ');
-}
-}
-putchar('\n');
-return (0);
+  
+  int i;
+
+  for (i = 0; i < 100 ; i++)
+  {
+    putchar((i / 10) + '0');
+    putchar((i % 10) + '0');
+    if (i != 99)
+    {
+      putchar(',');
+      putchar(' ');
+    }
+  }
+
+  putchar('\n');
+  return (0);
 }
