5ab927b6c36f8fb765c869bdcea702fb09df1d24
edgg72 <edgg72@hotmail.com>
2020-08-28 12:35:28 -0500

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,21 +8,24 @@
 
 void jack_bauer(void)
 {
-int hour, minute, a, b, c, d;
-for (hour = 0; hour < 24; hour++)
-{
-a = hour / 10;
-b = hour % 10;
-for (minute = 0; minute < 60; minute++)
-{
-c = minute / 10;
-d = minute % 10;
-_putchar('0' + a);
-_putchar('0' + b);
-_putchar(':');
-_putchar('0' + c);
-_putchar('0' + d);
-_putchar('\n');
-}
-}
+  
+  int hour, minute, a, b, c, d;
+
+  for (hour = 0; hour < 24; hour++)
+  {
+    a = hour / 10;
+    b = hour % 10;
+    
+    for (minute = 0; minute < 60; minute++)
+    {
+      c = minute / 10;
+      d = minute % 10;
+      _putchar('0' + a);
+      _putchar('0' + b);
+      _putchar(':');
+      _putchar('0' + c);
+      _putchar('0' + d);
+      _putchar('\n');
+    }
+  }
 }
