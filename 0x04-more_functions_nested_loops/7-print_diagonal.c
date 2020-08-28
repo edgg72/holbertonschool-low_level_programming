8f1126e00cefbda775b09712bef712afb5082f58
edgg72 <edgg72@hotmail.com>
2020-08-28 13:27:21 -0500

    Update 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,44 +7,43 @@
 
 void print_diagonal(int n)
 {
-int x, j, z;
-x = 0;
-j = n - (n - 1);
-z = 0;
-if (n > 0)
-{
-while (x < j)
-{
-if (x < 1)
-{
-_putchar(92);
-}
-else
-{
-z = 0;
-while (z < j)
-{
-if (z == (j - 1))
-{
-_putchar(92);
-}
-else
-{
-_putchar(' ');
-}
-z++;
-}
-}
-x++;
-if (j < n)
-{
-j++;
-}
-_putchar('\n');
-}
-}
-else
-{
-_putchar('\n');
-}
+
+  int x, j, z;
+  x = 0;
+  j = n - (n - 1);
+  z = 0;
+
+  if (n > 0)
+  {
+    while (x < j)
+    {
+      if (x < 1)
+      {
+        _putchar(92);
+      }
+      else
+      {
+        z = 0;
+        while (z < j)
+        {
+          if (z == (j - 1))
+            _putchar(92);
+        else
+        {
+          _putchar(' ');
+          z++;
+        }    
+      }
+    }
+    x++;
+    if (j < n)
+    {
+      j++;
+    }
+    _putchar('\n');
+  }
+  else
+  {
+    _putchar('\n');
+  }
 }
