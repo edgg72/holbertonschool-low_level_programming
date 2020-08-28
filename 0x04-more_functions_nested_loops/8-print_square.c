f2d717b9ad3d94d3d0cf136f9afb4b6e378f4266
edgg72 <edgg72@hotmail.com>
2020-08-28 13:28:53 -0500

    Update 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,22 +7,24 @@
 
 void print_square(int size)
 {
-if (size > 0)
-{
-int i, j;
-i = 0;
-j = 0;
-for (i = 0; i < size; i++)
-{
-for (j = 0; j < size; j++)
-{
-_putchar(35);
-}
-_putchar('\n');
-}
-}
-else
-{
-_putchar('\n');
-}
+  if (size > 0)
+  {
+    int i, j;
+    i = 0;
+    j = 0;
+
+    for (i = 0; i < size; i++)
+    {
+
+      for (j = 0; j < size; j++)
+      {
+        _putchar(35);
+      }
+      _putchar('\n');
+    }
+  }
+  else
+  {
+    _putchar('\n');
+  }
 }
