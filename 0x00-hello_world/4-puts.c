1b7f8603689131567b0c475275e0eb67075567ed
edgg72 <edgg72@hotmail.com>
2020-08-28 16:54:36 +0000

    aesthetic

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 /**
 *main - using puts for exercise 4
 *Return: - returning an int
 */
+
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,16 +6,16 @@
 
 int main(void)
 {
-  char charexample;
-  int intexample;
-  long longexample;
-  long long llexample;
-  float floatexample;
+	char charexample;
+	int intexample;
+	long longexample;
+	long long llexample;
+	float floatexample;
 
-  printf("Size of a char: %ld byte(s)\n", sizeof(charexample));
-  printf("Size of an int: %ld byte(s)\n", sizeof(intexample));
-  printf("Size of a long int: %ld byte(s)\n", sizeof(longexample));
-  printf("Size of a long long int: %ld byte(s)\n", sizeof(llexample));
-  printf("Size of a float: %ld byte(s)\n", sizeof(floatexample));
-  return (0);
+	printf("Size of a char: %ld byte(s)\n", sizeof(charexample));
+	printf("Size of an int: %ld byte(s)\n", sizeof(intexample));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(longexample));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(llexample));
+	printf("Size of a float: %ld byte(s)\n", sizeof(floatexample));
+	return (0);
 }
