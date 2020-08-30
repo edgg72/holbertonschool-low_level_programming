9aa8643dd74538099cf5af5a83720dfe14b959bf
edgg72 <edgg72@hotmail.com>
2020-08-30 15:42:32 -0500

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,24 +10,25 @@
 
 int main(int argc, char *argv[])
 {
-int result = 0;
-int  i;
-int j;
-for (i = 1; i < argc; i++)
-{
-for (j = 0; argv[i][j]; j++)
-{
-if (isdigit(argv[i][j]) == 0)
-{
-puts("Error");
-return (1);
-}
-}
-}
-for (i = 1; i < argc; i++)
-{
-result += atoi(argv[i]);
-}
-printf("%d\n", result);
-return (0);
+  int result = 0;
+  int  i;
+  int j;
+
+  for (i = 1; i < argc; i++)
+  {
+    for (j = 0; argv[i][j]; j++)
+    {
+      if (isdigit(argv[i][j]) == 0)
+      {
+        puts("Error");
+        return (1);
+      }
+    }
+  }
+  for (i = 1; i < argc; i++)
+  {
+    result += atoi(argv[i]);
+  }
+  printf("%d\n", result);
+  return (0);
 }
