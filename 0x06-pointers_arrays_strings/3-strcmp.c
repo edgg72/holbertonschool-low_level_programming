b4b0d62d9fe2d91bd8529ced663b85435026a18f
edgg72 <edgg72@hotmail.com>
2020-08-30 15:26:01 -0500

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,27 +9,28 @@
 
 int _strcmp(char *s1, char *s2)
 {
-int result = 0;
-while (*s1 != '\0')
-{
-if (*s1 > *s2)
-{
-result = (*s1 - *s2);
-return (result);
-}
-if (*s2 > *s1)
-{
-result = (*s2 - *s1);
-result = result *(-1);
-return (result);
-}
-s1++;
-s2++;
-}
-if (*s2 != '\0')
-{
-result = 0;
-return (result);
-}
-return (result);
+  int result = 0;
+
+  while (*s1 != '\0')
+  {
+    if (*s1 > *s2)
+    {
+      result = (*s1 - *s2);
+      return (result);
+    }
+    if (*s2 > *s1)
+    {
+      result = (*s2 - *s1);
+      result = result *(-1);
+      return (result);
+    }
+    s1++;
+    s2++;
+  }
+  if (*s2 != '\0')
+  {
+    result = 0;
+    return (result);
+  }
+  return (result);
 }
