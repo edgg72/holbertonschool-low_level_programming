3f5b08a221302d0fe5d98e480c3e1b37c7b48209
edgg72 <edgg72@hotmail.com>
2020-08-30 15:41:06 -0500

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,15 +10,17 @@
 
 int main(int argc, char *argv[])
 {
-int num1, num2, r;
-if (argc != 3)
-{
-puts("Error");
-return (1);
-}
-num1 = atoi(argv[1]);
-num2 = atoi(argv[2]);
-r = num1 *num2;
-printf("%d\n", r);
-return (0);
+  int num1, num2, r;
+
+  if (argc != 3)
+  {
+    puts("Error");
+    return (1);
+  }
+
+  num1 = atoi(argv[1]);
+  num2 = atoi(argv[2]);
+  r = num1 *num2;
+  printf("%d\n", r);
+  return (0);
 }
