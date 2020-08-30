a38845b1cb26805e39cdb66b7e920d8281e09877
edgg72 <edgg72@hotmail.com>
2020-08-30 15:18:40 -0500

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -8,11 +8,12 @@
 
 void _puts(char *str)
 {
-int x = 0;
-while (*(str + x) != '\0')
-{
-_putchar(str[x]);
-x++;
-}
-_putchar('\n');
+  int x = 0;
+
+  while (*(str + x) != '\0')
+  {
+    _putchar(str[x]);
+    x++;
+  }
+  _putchar('\n');
 }
