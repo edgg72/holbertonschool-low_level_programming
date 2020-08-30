6171a90d56d6a2033454aceb31a52c7f64f1d439
edgg72 <edgg72@hotmail.com>
2020-08-30 16:19:41 -0500

    Update 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -10,11 +10,11 @@
  */
 unsigned int _strlen(char *str)
 {
-unsigned int i;
+    unsigned int i;
 
-for (i = 0; str[i]; i++)
-;
-return (i);
+    for (i = 0; str[i]; i++)
+        ;
+    return (i);
 }
 
 /**
@@ -24,31 +24,37 @@ return (i);
  *
  * Return: pointer to the new node
  */
+
 list_t *add_node_end(list_t **head, const char *str)
 {
-list_t *new, *aux;
 
-if (str == NULL)
-return (NULL);
-new = malloc(sizeof(list_t));
-if (new == NULL)
-return (NULL);
-new->str = strdup(str);
-if (new->str == NULL)
-{
-free(new);
-return (NULL);
-}
-new->len = _strlen(new->str);
-new->next = NULL;
-if (*head == NULL)
-{
-*head = new;
-return (new);
-}
-aux = *head;
-while (aux->next)
-aux = aux->next;
-aux->next = new;
-return (new);
+    list_t *new, *aux;
+
+    if (str == NULL)
+        return (NULL);
+
+    new = malloc(sizeof(list_t));
+    if (new == NULL)
+        return (NULL);
+    new->str = strdup(str);
+    if (new->str == NULL)
+    {
+        free(new);
+        return (NULL);
+    }
+
+    new->len = _strlen(new->str);
+    new->next = NULL;
+
+    if (*head == NULL)
+    {
+        *head = new;
+        return (new);
+    }
+
+    aux = *head;
+    while (aux->next)
+    aux = aux->next;
+    aux->next = new;
+    return (new);
 }
