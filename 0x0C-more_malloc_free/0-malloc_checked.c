feaa3615c152645d2bd3a0b125f1a6ab6018c401
edgg72 <edgg72@hotmail.com>
2020-08-30 15:49:25 -0500

    Update 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -11,11 +11,11 @@
 void *malloc_checked(unsigned int b)
 {
 
-int *p;
-p = malloc(b);
-if (p == NULL)
-{
-exit(98);
-}
-return (p);
+  int *p;
+
+  p = malloc(b);
+  if (p == NULL)
+    exit(98);
+
+  return (p);
 }
