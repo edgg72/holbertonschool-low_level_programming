177cfdb67be2acda942052a9c70a5e67cd0c4595
edgg72 <edgg72@hotmail.com>
2020-08-30 15:33:11 -0500

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,23 +11,24 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-unsigned int i = 0, j = 0;
-while (haystack[i])
-{
-while (needle[j] != '\0' && (haystack[i] == needle[0]))
-{
-if (haystack[i + j] == needle[j])
-j++;
-else
-break;
-}
-if (needle[j] != '\0')
-{
-i++;
-j = 0;
-}
-else
-return (haystack + i);
-}
-return (0);
+  unsigned int i = 0, j = 0;
+
+  while (haystack[i])
+  {
+    while (needle[j] != '\0' && (haystack[i] == needle[0]))
+    {
+      if (haystack[i + j] == needle[j])
+        j++;
+      else
+        break;
+    }
+    if (needle[j] != '\0')
+    {
+      i++;
+      j = 0;
+    }
+    else
+      return (haystack + i);
+  }
+  return (0);
 }
