04c0ca8b4f0d43bc87f582702c0a830bcb734302
edgg72 <edgg72@hotmail.com>
2021-01-14 08:31:46 -0500

    changing while to for

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -15,12 +15,11 @@ int linear_search(int *array, size_t size, int value)
 	if (array == NULL)
 		return (-1);
 
-	while (i < (int)size)
+	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
-		i++;
 	}
 
 	return (-1);
